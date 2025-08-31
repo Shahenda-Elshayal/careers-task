7cedf515b42681f21faaf6502fa4a450da418456
Shahenda Elshayal <shahenda.elshayal@gmail.com>
2025-08-31 22:19:03 +0300

    style: put an image as bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import "./App.css";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
+import BgImg from "./assets/images/bgImg.jpg";
 
 export default function App() {
   return (
-    <>
+    // <div className="bg-gradient-to-r from-[#1F1F1F] from-25% via-[#FFBDE8] via-40% via-[#BDFFDF] via-50% to-[#1F1F1F] to-90%">
+    <div style={{ backgroundImage: `url(${BgImg})` }} className="bg-cover">
       <Navbar />
-      <div className="grid grid-cols-12 px-4 md:px-0 ">
-        <div className="col-span-12 col-start-1 md:col-start-2 md:col-span-10 lg:col-start-3 lg:col-span-8">
-        </div>
-      </div>
-          <HeroSection/>
-    </>
+      {/* <div className="grid grid-cols-12 px-4 md:px-0">
+        <div className="col-span-12 col-start-1 md:col-start-2 md:col-span-10 lg:col-start-3 lg:col-span-8"></div>
+      </div> */}
+      <HeroSection />
+    </div>
   );
 }
