d9d9f3840f5298c307c9aad4cbe0e652dee25367
Shahenda Elshayal <shahenda.elshayal@gmail.com>
2025-09-01 07:03:38 +0300

    feat: finish price-section

--- src/components/PriceSection.jsx
+++ src/components/PriceSection.jsx
@@ -3,6 +3,9 @@ import PriceCard from "./PriceCard";
 import Custom from "../assets/images/padge-logo/custom.png";
 import Heart from "../assets/images/padge-logo/heart.png";
 import Stars from "../assets/images/padge-logo/stars.png";
+import BgImg from "../assets/images/bg-pricing.png";
+import ListIcon from "../assets/images/list.png";
+import PlusIcon from "../assets/images/plus.png";
 
 export default function PriceSection() {
   const listItems = [
@@ -31,7 +34,10 @@ export default function PriceSection() {
     ],
   ];
   return (
-    <div className="grid grid-cols-12 px-4 md:px-0 pt-[120px] pb-[120px] text-center">
+    <div
+      className="grid grid-cols-12 px-4 md:px-0 pt-[120px] pb-[120px] text-center"
+      style={{ backgroundImage: `url(${BgImg})` }}
+    >
       <div className="col-span-12 col-start-1 md:col-start-2 md:col-span-10 lg:col-start-3 lg:col-span-8">
         {/* headers of pricing */}
         <div className="mb-14">
@@ -70,17 +76,90 @@ export default function PriceSection() {
             listItems={listItems[1]}
             vip={true}
           />
-          <PriceCard 
+          <PriceCard
             bgColor="bg-transparent"
             PadgeSrc={Custom}
-           padgeTitle="ENTERPRISE"
-           price="Custom"
-           PadgeBg="bg-gradient-to-r from-[#C070D84D] to-[#1717174D]"
-           title="For large scale models with large and ever-changing context."
-           btnTitle="Request Access"
-           bgTextColor="text-white"
-           bgBtn="bg-gradient-to-b from-[#1B1B1D] to-[#1B1B1B]"
-           listItems={listItems[2]} />
+            padgeTitle="ENTERPRISE"
+            price="Custom"
+            PadgeBg="bg-gradient-to-r from-[#C070D84D] to-[#1717174D]"
+            title="For large scale models with large and ever-changing context."
+            btnTitle="Request Access"
+            bgTextColor="text-white"
+            bgBtn="bg-gradient-to-b from-[#1B1B1D] to-[#1B1B1B]"
+            listItems={listItems[2]}
+          />
+        </div>
+        {/* dedicated support */}
+        <div className="mt-[20px] border-2 border-[#25252733] rounded-2xl flex flex-col md:flex-row">
+          {/* right div */}
+          <div className="bg-gradient-to-r rounded-l-2xl from-[#111112] via-[#25252733] to-[#1B1B1D] p-[14px] flex-1">
+            <div className="pt-[27px] pb-[27px] pl-[20px] pr-[20px] border-2 border-[#25252733] rounded-tl-2xl rounded-bl-2xl text-left">
+              <h3 className="font-[400] text-[28px] mb-3 text-white">
+                Dedicated Support
+              </h3>
+              <p className="text-white opacity-50 text-[16px] font-[400] mb-5">
+                We are here to help get you started with a dedicated <br />{" "}
+                support engineer who can assist with scoping your first <br />{" "}
+                models and getting them deployed.
+              </p>
+              <p className="uppercase text-[13px] font-[400] text-white opacity-80 mb-5">
+                What’s included
+              </p>
+              <div className="grid grid-cols-1 lg:grid-cols-2 ">
+                <div className="flex items-center gap-3">
+                  <img src={ListIcon} alt="List Icon" />
+                  <span className="text-[16px] font-[400] text-white opacity-80">
+                    Shared Slack Channel
+                  </span>
+                </div>
+                <div className="flex items-center gap-3">
+                  <img src={ListIcon} alt="List Icon" />
+                  <span className="text-[16px] font-[400] text-white opacity-80">
+                    Dedicated Support <br /> Engineer
+                  </span>
+                </div>
+                <div className="flex items-center gap-3">
+                  <img src={ListIcon} alt="List Icon" />
+                  <span className="text-[16px] font-[400] text-white opacity-80">
+                    Prompt Engineering <br /> Guidance
+                  </span>
+                </div>
+                <div className="flex items-center gap-3">
+                  <img src={ListIcon} alt="List Icon" />
+                  <span className="text-[16px] font-[400] text-white opacity-80">
+                    Context Sourcing Guidance
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* left div */}
+          <div
+            className="p-3 text-center"
+            style={{
+              background:
+                "linear-gradient(to bottom, #0C0A0B 50%, #0C0A0B 50%)",
+            }}
+          >
+            <div className="pt-7 pb-7 pl-5 pr-5 border-2 border-[#25252733] rounded-tr-xl rounded-br-xl">
+              <div
+                className={`w-fit mx-auto rounded-2xl bg-gradient-to-r from-[#C070D84D] to-[#1717174D] border border-[#FFFFFF0F] text-[#CECECE] font-[400] text-[13px] pt-1 pb-1 pl-1 pr-3 flex gap-2 items-center mb-3 cursor-pointer`}
+              >
+                <img src={PlusIcon} alt="Plus Logo" />
+                ADD ON
+              </div>
+              <p className="text-white font-[400] text-4xl mb-1">$750</p>
+              <p className="text-white font-[400] text-[16px] opacity-80 mb-6">
+                monthly
+              </p>
+              <button className="w-[240px] rounded-[26px] text-[#09090A] text-[14px] font-medium bg-white mb-3 p-2">
+                Request Access
+              </button>
+              <p className="text-white text-left font-[400] text-[12px] opacity-50 ">
+                No long term contract obligation.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
